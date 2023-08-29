1223b8a7d88c7a4a627d2299011afb7ae490bf6e
Axel Jacobsen <axelnj44@gmail.com>
2023-08-29 21:34:29 +0100

    progress

--- sudoku.py
+++ sudoku.py
@@ -2,7 +2,8 @@
 
 import unittest
 
-from typing import List, Optional
+from copy import deepcopy
+from typing import List, Tuple, Optional
 
 
 class Sudoku:
@@ -11,10 +12,15 @@ class Sudoku:
             [None for i in range(9)] for j in range(9)
         ]
 
+    def copy(self):
+        s = Sudoku()
+        s.set_grid(deepcopy(self.grid))
+
     def __getitem__(self, i: int, j: int) -> Optional[int]:
         return self.grid[i][j]
 
-    def __setitem__(self, i: int, j: int, val: int) -> None:
+    def __setitem__(self, pos: Tuple[int,int], val: int) -> None:
+        i,j = pos
         self.grid[i][j] = val
 
     def set_grid(self, grid: List[List[Optional[int]]]) -> None:
@@ -24,13 +30,13 @@ class Sudoku:
         if not (0 <= row < 9):
             raise ValueError(f"row index {row} out of bounds")
 
-        return set(self.grid[row]) == set(range(1, 10))
+        return set(range(1, 10)) - set(self.grid[row]) == set()
 
     def check_col(self, col: int) -> bool:
         if not (0 <= col < 9):
             raise ValueError(f"column index {col} out of bounds")
 
-        return set([self.grid[i][col] for i in range(9)]) == set(range(1, 10))
+        return set(range(1, 10)) - set([self.grid[i][col] for i in range(9)]) == set()
 
     def check_square(self, row: int, col: int) -> bool:
         if not (0 <= row < 3 and 0 <= col < 3):
@@ -39,7 +45,7 @@ class Sudoku:
         elements = [
             self.grid[3 * row + i][3 * col + j] for i in range(3) for j in range(3)
         ]
-        return set(elements) == set(range(1, 10))
+        return set(range(1, 10)) - set(elements) == set()
 
     def check_valid(self) -> bool:
         for i in range(9):
@@ -51,8 +57,37 @@ class Sudoku:
                     return False
         return True
 
+    def check_solved(self) -> bool:
+        return self.check_valid() and len(self.get_empty_grid_positions()) == 0
+
     def get_empty_grid_positions(self) -> List[List[int]]:
         return [[i, j] for i in range(9) for j in range(9) if self.grid[i][j] is None]
+
+def solve(board) -> Optional["Sudoku"]:
+    """
+    There is a nice recursive solution here
+
+    while not solved:
+        pick the first empty slot
+        for numbers in 1..9
+            if it is valid, try solve with new grid
+                if solve with new grid returns a board, return board
+                else, continue
+            if it is invalid, continue
+        return false
+    """
+    if board.check_solved():
+        return board
+
+    row, col = board.get_empty_grid_positions().pop()
+    for candidate in range(1,10):
+        board[row, col] = candidate
+        if board.check_valid():
+            copy = board.copy()
+            candidate_board = copy.solve()
+            if candidate_board is not None:
+                return candidate_board
+    return None  # no valid solutions
 
 
 class SudokuBoardTest(unittest.TestCase):
@@ -140,6 +175,42 @@ class SudokuBoardTest(unittest.TestCase):
             ]
         )
         self.assertFalse(s.check_valid())
+
+    def test_basic_solve_1(self):
+        s = Sudoku()
+        s.set_grid(
+            [
+                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                [4, 5, 6, 7, 8, 9, 1, 2, 3],
+                [7, 8, 9, 1, 2, 3, 4, 5, 6],
+                [2, 3, 4, 5, 6, 7, 8, 9, 1],
+                [5, 6, 7, 8, 9, 1, 2, 3, 4],
+                [8, 9, 1, 2, 3, 4, 5, 6, 7],
+                [3, 4, 5, 6, 7, 8, 9, 1, 2],
+                [6, 7, 8, 9, 1, 2, 3, 4, 5],
+                [9, 1, 2, 3, 4, 5, 6, 7, None],
+            ]
+        )
+        solved = solve(s)
+        self.assertTrue(solved.check_valid())
+
+    def test_basic_solve_2(self):
+        s = Sudoku()
+        s.set_grid(
+            [
+                [None, None, 8, 1, 7, 3, None, None, None],
+                [7, 3, 9, 5, 6, 2, 8, 4, 1],
+                [5, 6, 1, 9, 8, 4, 7, 3, 2],
+                [3, 5, 4, 6, 2, 8, 1, 7, 9],
+                [9, 8, 7, 3, 5, 1, 2, 6, 4],
+                [6, 1, 2, 7, 4, 9, 5, 8, 3],
+                [2, 7, 5, 4, 9, 6, 3, None, None],
+                [1, 9, 6, 8, 3, 5, 4, None, None],
+                [8, 4, 3, 2, 1, 7, 9, None, None],
+            ]
+        )
+        solved = solve(s)
+        self.assertTrue(solved.check_valid())
 
 
 if __name__ == "__main__":
