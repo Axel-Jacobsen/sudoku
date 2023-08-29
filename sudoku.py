b801f1bedec186ea52f348d5822b36836ff3f037
Axel Jacobsen <axelnj44@gmail.com>
2023-08-29 20:24:54 +0100

    add incomplete

--- sudoku.py
+++ sudoku.py
@@ -119,6 +119,24 @@ class SudokuBoardTest(unittest.TestCase):
         )
         self.assertFalse(s.check_valid())
 
+    def test_incomplete_1(self):
+        s = Sudoku()
+        s.set_grid(
+            [
+                [1, 2, 8, 1, 7, 3, 6, 9, 5],
+                [7, 3, 9, 5, 6, 2, 8, 4, 1],
+                [5, 6, 1, 9, 8, 4, 7, 3, 2],
+                [3, 5, 4, 6, 2, 8, 1, 7, 9],
+                [9, 8, 7, 3, 5, 1, 2, 6, 4],
+                [6, 1, 2, 7, 4, 9, 5, 8, 3],
+                [2, 7, 5, 4, 9, 6, 3, 1, 8],
+                [1, 9, 6, 8, 3, 5, 4, 2, 7],
+                [8, 4, 3, 2, 1, 7, 9, 5, None],
+            ]
+        )
+        self.assertFalse(s.check_valid())
+
+
 
 if __name__ == "__main__":
     unittest.main()
